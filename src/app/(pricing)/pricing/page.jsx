b40b66c4c8c2e1703354ae3595683b98cc8770ea
Suraj Pandit <suraejpaedt@gmail.com>
2025-06-24 12:06:13 +0545

    fix

--- src/app/(pricing)/pricing/page.jsx
+++ src/app/(pricing)/pricing/page.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import Hero from './components/Hero'
 import ContactUs from '@/app/(frontend)/components/sections/ContactUs'
+import AboutUs from '@/app/(frontend)/components/sections/AboutUs';
+import WhyChooseUs from '@/app/(frontend)/components/sections/WhyChooseUs';
 
 const page = () => {
   return (
     <div>
-        <Hero />
-        <section id="contact" className="section-offset">
+      <Hero />
+      <section id="about" className="section-offset">
+        <AboutUs />
+      </section>
+      <section id="contact" className="section-offset">
         <ContactUs />
-        </section>
+      </section>
+      <section id="whychooseus" className="section-offset">
+        <WhyChooseUs />
+      </section>
     </div>
-  )
+  );
 }
 
 export default page
